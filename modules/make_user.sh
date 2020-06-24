2370c95010eacd2ba091a9308927a34422d45ef9
Thomas Castleman <contact@draugeros.org>
2020-06-24 00:58:40 -0400

    live user does not exist
    since a live user doesn't exist, we need to make a user from scratch

--- modules/make_user.sh
+++ modules/make_user.sh
@@ -23,32 +23,17 @@
 #
 USERNAME="$1"
 PASSWORD="$2"
-function fix_home () 
 {
-	{
-		USERNAME="$1"
-		if [ -d /home/home/live ]; then
-			mv -v /home/home/live /home/"$USERNAME"
-			rm -vrf /home/home
-		fi
-	} 1>&2
-}
-
-#change live user to $USERNAME
-usermod -l "$USERNAME" live 1>&2
-groupmod -n "$USERNAME" live 1>&2
-if $(ls /home | grep -q "$USERNAME"); then
-	#check to see if the user has a home folder already. 
-	rm -rfv /home/live 1>&2
-elif [ -d /home/home/live ]; then
-	fix_home "$USERNAME"
-else
+	useradd --create-home --system --shell /bin/bash --groups adm,cdrom,sudo,audio,dip,plugdev,lpadmin "$USERNAME"
+} || {
+	#change live user to $USERNAME
+	usermod -l "$USERNAME" live 1>&2
+	groupmod -n "$USERNAME" live 1>&2
 	#change refrences from old home to new
 	sed -i "s:/home/live:/home/$USERNAME:g" /home/live/.config/gtk-3.0/bookmarks 1>&2
 	#rename home directory
-	mv -v /home/live /home/"$USERNAME" 1>&2 || fix_home "$USERNAME"
-fi
-sed -i "s/live/$USERNAME/g" /etc/passwd  1>&2
+	mv -v /home/live /home/"$USERNAME" 1>&2
+	sed -i "s/live/$USERNAME/g" /etc/passwd  1>&2
 }
 #change password
 builtin echo "$USERNAME:$PASSWORD" | chpasswd
